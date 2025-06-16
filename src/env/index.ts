9d90cbc87f077c2df23dcf2477fa2bef29ffe38f
pdro-h0 <pedro.pho792@gmail.com>
2025-06-16 18:58:35 -0300

    fix: ensure port has default value

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["test", "development", "production"])
     .default("development"),
-  PORT: z.coerce.number(),
+  PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
 })
 export const env = envSchema.parse(process.env)
